900507892aff077afb25aac9402e31b7b1e787bf
hari <harilr1001@gmail.com>
2022-03-11 15:36:51 +0530

    ARI PROGRAM and Test Case

---
+++ ari.py
@@ -0,0 +1,43 @@
+import math
+
+def charCount(s):
+    cCount = 0
+    for i in s:
+        if i.isalnum():
+            cCount += 1
+    return cCount
+
+def wordCount(s):
+    wCount = 0
+    for i in s:
+        if i == " ":
+           wCount += 1
+    return wCount
+
+def sentenceCount(s):
+    return s.count('!') + s.count('.') + s.count("?")
+
+def autoreadIndex_Calculate(s):
+    chars = charCount(string)
+    words = wordCount(string)
+    sentences = sentenceCount(string)
+
+    ari_result = 4.71 *(chars/words) + 0.5*(words/sentences)
+    return math.ceil(ari_result)
+
+string = """The Automated Readability Index is derived from ratios representing word difficulty (number of letters per word) 
+and sentence difficulty (number of words per sentence).The first consideration in developing the Automated Readability Index was 
+to establish that the factors used relate to those found in other indices!? The factor relating to sentence structure 
+(average number of words per sentence) is identical to that found in most currently used indices , such as the Coleman-Liau Index,
+so no verification was required!. The verification of the relationship between the word structure factor was also virtually self-evident!.
+Most readability indices consist of two factors!. One factor relates to sentence structure and is most generally a measure of the average 
+number of words per sentence!.The other factor generally relates to word structure and is usually based on either the proportion of easy 
+words determined with reference to word list (Dale and Chall, 1948) or the average number of syllables per word (Flesch, 1951).
+While the word list has many advantages, especially in 4th lower grades, it is both slow? and relatively inaccurate when applied to adult 
+reading material!. Syllable counts prove to be deceptively unreliable!.""" 
+
+print(charCount(string))
+print(wordCount(string))
+print(sentenceCount(string))
+print(autoreadIndex_Calculate(string))
+
---
+++ test_ari.py
@@ -0,0 +1,46 @@
+from ari import *
+
+
+def test_char_count_None():
+    ret = charCount("")
+    assert ret == 0
+
+def test_char_count_Invalidchar():
+    ret = charCount('"~!@#$%^&*()_+*/-{}''[]|;:`=')
+    assert ret == 0
+
+def test_char_count_Multiple():
+    ret = charCount("hello there 12345")
+    assert ret == 15
+
+def test_word_count_None():
+    ret = wordCount("abcd")
+    assert ret == 0
+
+def test_word_count_Multiple():
+    ret = wordCount("abcd 1234 wxyz 5678")
+    assert ret == 3
+
+
+def test_sentence_count_None():
+    ret = sentenceCount("abcd1234hello")
+    assert ret == 0
+    
+def test_sentence_count_Multiple():
+    ret = sentenceCount("abcd.1234?hello!")
+    assert ret == 3
+
+def test_ari_calculate_score_greater14():
+    ret = autoreadIndex_Calculate("Hello! How are you?") > 14
+    assert ret == True
+
+def test_ari_calculate_score14_orless():
+    ret = autoreadIndex_Calculate("Hello! How are you?") <= 14
+    assert ret == False
+
+
+
+    
+    
+
+
