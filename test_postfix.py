cb78b955825f035455c810ca60b6edc57194e2f4
hari <harilr1001@gmail.com>
2022-03-14 12:52:04 +0530

    Added Test case for valid postfix_expression evaluation

--- test_postfix.py
+++ test_postfix.py
@@ -4,3 +4,11 @@ from postfix_eval import postfix_calc
 def test_postfix_empty():
     ret = postfix_calc("")
     assert ret == None
+
+def test_postfix_expression_valid():
+    ret = postfix_calc("123++")
+    assert ret == 6
+
+def test_postfix_expression_string():
+    ret = postfix_calc("abc++")
+    assert ret == None
